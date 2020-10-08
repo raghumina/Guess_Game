ee669a7ff16bed1fb24bb2c79ac35b4eff41050f
raghu <raghumina11@gmail.com>
2020-10-08 22:42:04 +0530

    Create Python_Fun_game.py

---
+++ Python_Fun_game.py
@@ -0,0 +1,18 @@
+# Python fun game
+# Python Project 1
+# Using Basics of python
+# Modules used If , else, elif, list etc
+
+
+# Lets start
+
+print("----------WELCOME TO MY FIRST GAME-------------")
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+health = 10
+
+if age >= 18:
+    print("You are old enough to play")
+elif age < 18:
+    print("Not allowed to play")
+    
